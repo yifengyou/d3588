6ba644ef35dfceb1e6eb3a828c43b6954e131259
yifengyou <842056007@qq.com>
2026-04-03 00:02:10 +0800

    更新到22.04构建环境
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdevbuild/official_5.10.198_kernel.sh
+++ kdevbuild/official_5.10.198_kernel.sh
@@ -10,30 +10,27 @@ export DEBIAN_FRONTEND=noninteractive
 #==========================================================================#
 apt-get update
 apt-get install -qq -y ca-certificates
-apt install -y --no-install-recommends \
-  acl android-tools-adb aptly aria2 autoconf autotools-dev axel bc \
-  binfmt-support binutils binutils-aarch64-linux-gnu bison \
-  btrfs-progs build-essential busybox ca-certificates ccache clang cmake \
-  coreutils cpio crossbuild-essential-arm64 cryptsetup curl cvs \
+apt-get install -qq -y --no-install-recommends \
+  acl aptly aria2 axel bc binfmt-support binutils-aarch64-linux-gnu bison \
+  bsdextrautils btrfs-progs build-essential busybox ca-certificates ccache \
+  clang coreutils cpio crossbuild-essential-arm64 cryptsetup curl \
   debian-archive-keyring debian-keyring debootstrap device-tree-compiler \
   dialog dirmngr distcc dosfstools dwarves e2fsprogs expect f2fs-tools \
-  fakeroot fdisk file flex gawk gcc gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi \
-  g++ gdisk git gnupg gzip htop imagemagick intltool jq kmod keychain \
-  lib32ncurses-dev lib32stdc++6 libbison-dev libc6-dev-armhf-cross libc6-i386 \
-  libcrypto++-dev libdrm-dev libelf-dev libfdt-dev libfile-fcntllock-perl \
-  libfl-dev libfuse-dev libgmp3-dev liblz4-tool python-is-python3 \
-  libmpc-dev libncurses-dev libncurses5 libncurses5-dev libncursesw5-dev \
-  libpython3-dev libsigsegv2 libssl-dev libtool libudev-dev \
-  libusb-1.0-0-dev linux-base lld llvm locales lsb-release lz4 lzma lzop m4 \
-  make mercurial minicom mtools ncurses-base ncurses-term net-tools \
-  nfs-kernel-server ntpdate openssh-client openssh-server openssl p7zip \
-  p7zip-full parallel parted patch patchutils pbzip2 perl pigz pixz \
-  pkg-config pv python3 python3-dev \
-  python3-distutils python3-pip python3-setuptools \
-  qemu-user-static rar rdfind rename ripgrep rsync sed squashfs-tools \
-  subversion sudo swig tar texinfo tree u-boot-tools udev unzip util-linux \
-  uuid uuid-dev uuid-runtime vim wget whiptail xfsprogs xsltproc xxd \
-  xz-utils zip zlib1g-dev zstd binwalk
+  fakeroot fdisk file flex gawk gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi \
+  gdisk git gnupg gzip htop imagemagick jq kmod lib32ncurses-dev \
+  lib32stdc++6 libbison-dev libc6-dev-armhf-cross libc6-i386 libcrypto++-dev \
+  libelf-dev libfdt-dev libfile-fcntllock-perl libfl-dev libfuse-dev \
+  libgcc-12-dev-arm64-cross libgmp3-dev liblz4-tool libmpc-dev libncurses-dev \
+  libncurses5 libncurses5-dev libncursesw5-dev libpython2.7-dev \
+  libpython3-dev libssl-dev libusb-1.0-0-dev linux-base lld llvm locales \
+  lsb-release lz4 lzma lzop make mtools ncurses-base ncurses-term \
+  nfs-kernel-server ntpdate openssl p7zip p7zip-full parallel parted patch \
+  patchutils pbzip2 pigz pixz pkg-config pv python2 python2-dev python3 \
+  python3-dev python3-distutils python3-pip python3-setuptools \
+  python-is-python3 qemu-user-static rar rdfind rename rsync sed \
+  squashfs-tools swig tar tree u-boot-tools udev unzip util-linux uuid \
+  uuid-dev uuid-runtime vim wget whiptail xfsprogs xsltproc xxd xz-utils \
+  zip zlib1g-dev zstd binwalk ripgrep sudo &>/dev/null
 
 if [ $? -ne 0 ]; then
   echo "install dependency failed"
